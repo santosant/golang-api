ea93cbc292f5c999b3f29b935c9b720c09d8a188
Antonio Anderson <hiroto.anderson@gmail.com>
2022-05-18 18:39:43 -0300

    handlers create and update created

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/santosant/go-api
 go 1.18
 
 require (
+	github.com/go-chi/chi/v5 v5.0.7
 	github.com/lib/pq v1.10.6
 	github.com/spf13/viper v1.11.0
 )
---
+++ handlers/update.go
@@ -0,0 +1,48 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/santosant/go-api/models"
+)
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		log.Printf("Error to parse id: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	var todo models.Todo
+
+	err = json.NewDecoder(r.Body).Decode(&todo)
+	if err != nil {
+		log.Printf("Erro decoding Json: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	rows, err := models.Update(int64(id), todo)
+	if err != nil {
+		log.Printf("Error to update todo id: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	if rows > 1 {
+		log.Printf("Error: So many rows: %d", rows)
+	}
+
+	resp := map[string]any{
+		"Error":   false,
+		"Message": "Data was update with successful",
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
+}
